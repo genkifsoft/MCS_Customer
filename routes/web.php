0e3977e4cc2266a7a343d55438b801db1f8e2afe
Dang Quy <dangquy95php@gmail.com>
2020-05-12 07:13:15 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.dashboard');
+});
+
+
+Route::post('auth/social', 'SocialAuthController@authSocial');
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('create-customer', 'CustomerController@getCreateCustomer');
+    Route::post('create-customer', 'CustomerController@createCustomer')->name('create_customer');
+    Route::post('login', 'CustomerController@loginCustomer');
+    Route::post('fogot-password', 'CustomerController@fogotPassword');
+
+    Route::group(['middleware' => 'jwt.auth'], function () {
+        Route::post('detail', 'CustomerController@detailCustomer');
+        Route::post('list', 'CustomerController@getAllCustomer');
+        Route::post('logout', 'CustomerController@logoutCustomer');
+        Route::post('delete', 'CustomerController@deleteCustomer');
+        Route::post('update', 'CustomerController@updateCustomer');
+        Route::post('refresh', 'CustomerController@refreshCustomer');
+        Route::post('change-pass', 'CustomerController@changePassword');
+    });
 });
